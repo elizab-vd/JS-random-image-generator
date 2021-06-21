be796f6e404b13eeb36ef44d6d1a4a3eb07ecdec
Eliza B <vivalterdesigns@gmail.com>
2021-06-21 16:22:13 -0400

    Fixed missing quotes

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
 const getImage = async function () {
-    const res = await fetch (https://picsum.photos/v2/list?limit=100);
+    const res = await fetch ("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
     console.log(images);
-}
+};
 
 getImage();
